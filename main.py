36300c692d0becf38e7f8662b5a9a39b38140310
TheRealAsciiMan <endvad.galaxy@gmail.com>
2023-10-25 13:50:40 +0200

    Gestion du login

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for
 import sqlite3
+
+
 def main():
     app = Flask(__name__)
+    app.secret_key = "(L)SD>HD"
     @app.route('/')
     def index():
         conn = sqlite3.connect('dataBase.db')
@@ -15,7 +18,43 @@ def main():
 
     @app.route('/ajout')
     def ajout():
-        return render_template("ajout.html")
+        if 'pseudo' in session and type(session["pseudo"]) == str:
+            return render_template("ajout.html")
+        else:
+            return redirect("/")
+
+    @app.route('/login')
+    def login():
+        return render_template("login.html")
+
+    @app.route('/action_login', methods=['POST', 'GET'])
+    def action_login():
+        if request.method == 'GET':
+            return redirect("/")
+        elif request.method == 'POST':
+            mail = request.form.get('mail')
+            password = request.form.get('password')
+            password = password[:4] + '-' + password[4:6] + '-' + password[6:8]
+            data = (mail, password)
+            conn = sqlite3.connect('dataBase.db')
+            cur = conn.cursor()
+            cur.execute('SELECT Pseudo, "Date d\'inscription"  FROM Users WHERE Mail = ? AND "Date d\'inscription" = ?', data)
+            conn.commit()
+            info = cur.fetchone()
+            cur.close()
+            conn.close()
+            if info:
+                session['pseudo'] = info[0]
+                return redirect("/")
+            else:
+                return redirect(url_for("login", error = True))
+    @app.route('/logout')
+    def logout():
+        session['pseudo'] = None
+        return redirect("/login")
+
+
+
 
     @app.route('/ajouter_livre', methods=['POST', 'GET'])
     def ajouter_livre():
@@ -34,6 +73,8 @@ def main():
             cur.close()
             conn.close()
             return redirect('/')
+
+
     app.run(debug=True, host='0.0.0.0', port=80)
 
 
